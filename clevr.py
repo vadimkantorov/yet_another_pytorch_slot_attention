d0211cc4838ea931fef368cc798d75522689699c
Vadim Kantorov <vadimkantorov@gmail.com>
2021-02-27 20:49:56 +0200

    ...

--- clevr.py
+++ clevr.py
@@ -8,9 +8,9 @@ class CLEVR(torchvision.datasets.VisionDataset):
         assert split_name in ['train', 'val', 'test']
         self.loader = loader
         scenes_json_path = os.path.join(root, 'scenes', f'CLEVR_{split_name}_scenes.json')
-        images_split_path = os.path.join(root, 'images', split_name)
+        images_split_dir = os.path.join(root, 'images', split_name)
         
-        self.image_paths = sorted(os.path.join(images_split_path, basename) for basename in os.listdir(images_split_path))
+        self.image_paths = sorted(os.path.join(images_split_dir, basename) for basename in os.listdir(images_split_dir))
         self.metadata = {s['image_filename'] : s['objects'] for s in json.load(open(scenes_json_path))['scenes'] } if os.path.exists(scenes_json_path) else { os.path.basename(image_path) : {} for image_path in self.image_paths}
     
         if filter != None:
