71567f6e0c4725bf6f63b6131ed1a716ef8a97e1
Vadim Kantorov <vadimkantorov@gmail.com>
2021-02-28 15:48:08 +0200

    ...

--- train.py
+++ train.py
@@ -41,7 +41,11 @@ def main(args):
 
     train_set = clevr.CLEVR(args.dataset_root_dir, 'train', filter = lambda scene_objects: len(scene_objects) <= 6)
     frontend = models.ImagePreprocessor(resolution = args.resolution, crop = args.crop)
-    model = models.SlotAttentionAutoEncoder(resolution = args.resolution, num_slots = args.num_slots, num_iterations = args.num_iterations, hidden_dim = args.hidden_dim).to(args.device)
+    model = models.SlotAttentionAutoEncoder(resolution = args.resolution, num_slots = args.num_slots, num_iterations = args.num_iterations, hidden_dim = args.hidden_dim)
+    if args.data_parallel:
+        model = nn.DataParallel(model)
+    model.to(args.device)
+
     criterion = nn.MSELoss()
     
     if args.checkpoint or args.checkpoint_tensorflow:
@@ -60,7 +64,7 @@ def main(args):
 
         total_loss = 0
 
-        for image_paths, images in train_dataloader:
+        for i, (image_paths, images) in enumerate(train_dataloader):
             iteration += 1
 
             learning_rate = (args.learning_rate * (iteration / args.warmup_steps) if iteration < args.warmup_steps else args.learning_rate) * (args.decay_rate ** (iteration / args.decay_steps))
@@ -78,37 +82,40 @@ def main(args):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            print(epoch, iteration, loss_item)
+            print('Epoch:', epoch, '#', iteration, '|', i, '/', len(train_dataloader), 'Loss:', loss_item)
 
         total_loss /= len(train_dataloader)
 
         print ('Epoch:', epoch, 'Loss:', total_loss, 'Time:', datetime.timedelta(seconds = time.time() - start))
 
-        if not epoch % 10:
-            torch.save(dict(model_state_dict = model.state_dict()), args.model_dir)
+        if not epoch % args.checkpoint_epoch_interval:
+            torch.save(dict(model_state_dict = model.state_dict()), os.path.join(args.model_dir, args.checkpoint_pattern.format(epoch = epoch)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_dir', default='./checkpoints', type=str, help='where to save models' )
+    parser.add_argument('--model-dir', default='./checkpoints', type=str, help='where to save models' )
     parser.add_argument('--seed', default=0, type=int, help='Random seed.')
-    parser.add_argument('--batch_size', default=64, type=int, help='Batch size for the model.')
-    parser.add_argument('--num_slots', default=7, type=int, help='Number of slots in Slot Attention.')
-    parser.add_argument('--num_iterations', default=3, type=int, help='Number of attention iterations.')
-    parser.add_argument('--num_train_steps', default=500000, type=int, help='Number of training steps.')
-    parser.add_argument('--learning_rate', default=0.0004, type=float, help='Learning rate.')
-    parser.add_argument('--warmup_steps', default=10000, type=int, help='Number of warmup steps for the learning rate.')
-    parser.add_argument('--decay_rate', default=0.5, type=float, help='Rate for the learning rate decay.')
-    parser.add_argument('--decay_steps', default=100000, type=int, help='Number of steps for the learning rate decay.')
+    parser.add_argument('--batch-size', default=64, type=int, help='Batch size for the model.')
+    parser.add_argument('--num-slots', default=7, type=int, help='Number of slots in Slot Attention.')
+    parser.add_argument('--num-iterations', default=3, type=int, help='Number of attention iterations.')
+    parser.add_argument('--num-train-steps', default=500000, type=int, help='Number of training steps.')
+    parser.add_argument('--learning-rate', default=0.0004, type=float, help='Learning rate.')
+    parser.add_argument('--warmup-steps', default=10000, type=int, help='Number of warmup steps for the learning rate.')
+    parser.add_argument('--decay-rate', default=0.5, type=float, help='Rate for the learning rate decay.')
+    parser.add_argument('--decay-steps', default=100000, type=int, help='Number of steps for the learning rate decay.')
 
-    parser.add_argument('--num_workers', default=16, type=int, help='number of workers for loading data')
-    parser.add_argument('--num_epochs', default=10000, type=int, help='number of workers for loading data')
-    parser.add_argument('--hidden_dim', default=64, type=int, help='hidden dimension size')
+    parser.add_argument('--data-parallel', action = 'store_true') 
+    parser.add_argument('--num-workers', default=16, type=int, help='number of workers for loading data')
+    parser.add_argument('--num-epochs', default=10000, type=int, help='number of workers for loading data')
+    parser.add_argument('--hidden-dim', default=64, type=int, help='hidden dimension size')
     parser.add_argument('--device', default = 'cuda', choices = ['cuda', 'cpu'])
     parser.add_argument('--resolution', type = int, nargs = 2, default = (128, 128))
     parser.add_argument('--crop', type = int, nargs = 4, default = (29, 221, 64, 256))
-    parser.add_argument('--dataset_root_dir', default = './CLEVR_v1.0')
+    parser.add_argument('--dataset-root-dir', default = './CLEVR_v1.0')
     parser.add_argument('--checkpoint')
-    parser.add_argument('--checkpoint_tensorflow')
+    parser.add_argument('--checkpoint-epoch-interval', type = int, default = 10)
+    parser.add_argument('--checkpoint-pattern', default = 'ckpt_{:04d}.pt')
+    parser.add_argument('--checkpoint-tensorflow')
     args = parser.parse_args()
 
     main(args)
