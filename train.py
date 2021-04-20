bfa49da84f193528c7cb080b7163e6916c205255
Vadim Kantorov <vadimkantorov@gmail.com>
2021-04-20 17:45:47 +0300

    ...

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def rename_and_transpose_tfcheckpoint(ckpt):
     return {k : v.permute(3, 2, 0, 1) if v.ndim == 4 else v.t() if v.ndim == 2 else v for k, v in ckpt.items()}
 
 def build_model(args):
-    model = models.SlotAttentionAutoEncoder(resolution = args.resolution, num_slots = args.num_slots, num_iterations = args.num_iterations, hidden_dim = args.hidden_dim).to(args.device)
+    model = models.SlotAttentionAutoEncoder(resolution = args.resolution, num_slots = args.num_slots, num_iter = args.num_iter, hidden_dim = args.hidden_dim).to(args.device)
         
     if args.checkpoint or args.checkpoint_tensorflow:
         model_state_dict = torch.load(args.checkpoint, map_location = 'cpu')['model_state_dict'] if args.checkpoint else rename_and_transpose_tfcheckpoint(torch.load(args.checkpoint_tensorflow, map_location = 'cpu')) 
@@ -57,7 +57,7 @@ def build_dataset(args, filter = None):
     assert os.path.exists(args.dataset_root_dir), f'provided dataset path [{args.dataset_root_dir}] does not exist'
     
     if args.dataset == 'CLEVR':
-        dataset = clevr.CLEVR(args.dataset_root_dir, args.split_name, filter = filter)
+        dataset = clevr.CLEVR(args.dataset_root_dir, args.dataset_split_name, filter = filter)
         batch_frontend = models.ClevrImagePreprocessor(resolution = args.resolution, crop = args.crop)
         collate_fn = torch.utils.data.dataloader.default_collate
 
@@ -66,7 +66,7 @@ def build_dataset(args, filter = None):
             train = (os.path.join(args.dataset_root_dir, f'train{args.coco_year}'), os.path.join(args.dataset_root_dir, 'annotations', f'{args.coco_mode}_train{args.coco_year}.json')),
             val = (os.path.join(args.dataset_root_dir, f'val{args.coco_year}'), os.path.join(args.dataset_root_dir, 'annotations', f'{args.coco_mode}_val{args.coco_year}.json')),
         )
-        dataset = coco.CocoDetection(*PATHS[args.split_name], transforms = models.CocoImagePreprocessorSimple(args.split_name), return_masks = args.coco_masks)
+        dataset = coco.CocoDetection(*PATHS[args.dataset_split_name], transforms = models.CocoImagePreprocessorSimple(args.dataset_split_name), return_masks = args.coco_masks)
         batch_frontend = nn.Identity()
         collate_fn = lambda batch: (torch.utils.data.dataloader.default_collate([t[0] for t in batch]), [t[1] for t in batch])
 
@@ -76,7 +76,7 @@ def build_criterion(args):
     return nn.MSELoss()
 
 def main(args):
-    os.makedirs(args.model_dir, exist_ok = True)
+    os.makedirs(args.checkpoint_dir, exist_ok = True)
  
     dataset, collate_fn, batch_frontend = build_dataset(args, filter = lambda scene_objects: len(scene_objects) <= 6)
     
@@ -120,38 +120,40 @@ def main(args):
 
         if not epoch % args.checkpoint_epoch_interval:
             model_state_dict = model.module.state_dict() if isinstance(model, nn.DataParallel) else model.state_dict()
-            torch.save(dict(model_state_dict = model_state_dict), os.path.join(args.model_dir, args.checkpoint_pattern.format(epoch = epoch)))
+            torch.save(dict(model_state_dict = model_state_dict), os.path.join(args.checkpoint_dir, args.checkpoint_pattern.format(epoch = epoch)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model-dir', default='./checkpoints', type=str, help='where to save models' )
-    parser.add_argument('--seed', default=0, type=int, help='Random seed.')
-    parser.add_argument('--batch-size', default=64, type=int, help='Batch size for the model.')
-    parser.add_argument('--num-slots', default=7, type=int, help='Number of slots in Slot Attention.')
-    parser.add_argument('--num-iterations', default=3, type=int, help='Number of attention iterations.')
+    
+    parser.add_argument('--num-epochs', default=10000, type=int, help='number of workers for loading data')
     parser.add_argument('--num-train-steps', default=500000, type=int, help='Number of training steps.')
+    parser.add_argument('--batch-size', default=64, type=int, help='Batch size for the model.')
+    parser.add_argument('--data-parallel', action = 'store_true') 
+    parser.add_argument('--device', default = 'cuda', choices = ['cuda', 'cpu'])
+    parser.add_argument('--num-workers', default=16, type=int, help='number of workers for loading data')
     parser.add_argument('--learning-rate', default=0.0004, type=float, help='Learning rate.')
     parser.add_argument('--warmup-steps', default=10000, type=int, help='Number of warmup steps for the learning rate.')
     parser.add_argument('--decay-rate', default=0.5, type=float, help='Rate for the learning rate decay.')
     parser.add_argument('--decay-steps', default=100000, type=int, help='Number of steps for the learning rate decay.')
-
-    parser.add_argument('--data-parallel', action = 'store_true') 
-    parser.add_argument('--num-workers', default=16, type=int, help='number of workers for loading data')
-    parser.add_argument('--num-epochs', default=10000, type=int, help='number of workers for loading data')
+    parser.add_argument('--seed', default=0, type=int, help='Random seed.')
+    
+    parser.add_argument('--num-iter', default=3, type=int, help='Number of attention iterations.')
+    parser.add_argument('--num-slots', default=7, type=int, help='Number of slots in Slot Attention.')
     parser.add_argument('--hidden-dim', default=64, type=int, help='hidden dimension size')
-    parser.add_argument('--device', default = 'cuda', choices = ['cuda', 'cpu'])
     parser.add_argument('--resolution', type = int, nargs = 2, default = (128, 128))
     parser.add_argument('--crop', type = int, nargs = 4, default = (29, 221, 64, 256))
+   
+    parser.add_argument('--checkpoint-dir', default='./checkpoints', type=str, help='where to save models' )
     parser.add_argument('--checkpoint')
     parser.add_argument('--checkpoint-epoch-interval', type = int, default = 10)
     parser.add_argument('--checkpoint-pattern', default = 'ckpt_{epoch:04d}.pt')
     parser.add_argument('--checkpoint-tensorflow')
+    
     parser.add_argument('--dataset', default = 'CLEVR', choices = ['CLEVR', 'COCO'])
     parser.add_argument('--dataset-root-dir', default = './CLEVR_v1.0')
-    parser.add_argument('--split-name', default = 'train', choices = ['train', 'val'])
+    parser.add_argument('--dataset-split-name', default = 'train', choices = ['train', 'val'])
     parser.add_argument('--coco-year', type = int, default = 2017)
     parser.add_argument('--coco-masks', action = 'store_true')
     parser.add_argument('--coco-mode', default = 'instances')
-    args = parser.parse_args()
-
-    main(args)
+    
+    main(parser.parse_args())
