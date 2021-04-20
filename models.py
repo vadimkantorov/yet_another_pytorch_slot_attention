cadba8479fa2dc93b4a74331cf1e14bb223666c7
Vadim Kantorov <vadimkantorov@gmail.com>
2021-04-20 17:36:46 +0300

    ...

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -161,7 +162,7 @@ class SlotAttentionDecoder(nn.Sequential):
         )
 
 
-class SoftPositionEmbed(nn.Module):
+class PositionEmbeddingImplicit(nn.Module):
     def __init__(self, hidden_dim):
         super().__init__()
         self.dense = nn.Linear(4, hidden_dim)
@@ -172,9 +173,67 @@ class SoftPositionEmbed(nn.Module):
         grid = torch.cat([grid, 1 - grid], dim = -1)
         return x + self.dense(grid)
 
+class PositionEmbeddingSine(nn.Module):
+    # https://github.com/facebookresearch/detr/blob/master/models/position_encoding.py
+    def __init__(self, num_pos_feats=64, temperature=10000, normalize=True, scale=None):
+        super().__init__()
+        self.num_pos_feats = num_pos_feats
+        self.temperature = temperature
+        self.normalize = normalize
+        if scale is not None and normalize is False:
+            raise ValueError("normalize should be True if scale is passed")
+        if scale is None:
+            scale = 2 * math.pi
+        self.scale = scale
+
+    def forward(self, x):
+        not_mask = torch.ones_like(x)
+        y_embed = not_mask.cumsum(1, dtype=torch.float32)
+        x_embed = not_mask.cumsum(2, dtype=torch.float32)
+        if self.normalize:
+            eps = 1e-6
+            y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
+            x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
+
+        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
+        dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
+
+        pos_x = x_embed[:, :, :, None] / dim_t
+        pos_y = y_embed[:, :, :, None] / dim_t
+        pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
+        pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
+        pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
+        return x + pos
+
+
+class PositionEmbeddingLearned(nn.Module):
+    # https://github.com/facebookresearch/detr/blob/master/models/position_encoding.py
+    def __init__(self, num_pos_feats=256):
+        super().__init__()
+        self.row_embed = nn.Embedding(50, num_pos_feats)
+        self.col_embed = nn.Embedding(50, num_pos_feats)
+        #TODO: assert that x.shape matches the passed row_embed, col_embed
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.uniform_(self.row_embed.weight)
+        nn.init.uniform_(self.col_embed.weight)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        i = torch.arange(w, device=x.device)
+        j = torch.arange(h, device=x.device)
+        x_emb = self.col_embed(i)
+        y_emb = self.row_embed(j)
+        pos = torch.cat([
+            x_emb.unsqueeze(0).repeat(h, 1, 1),
+            y_emb.unsqueeze(1).repeat(1, w, 1),
+        ], dim=-1).permute(2, 0, 1).unsqueeze(0).repeat(x.shape[0], 1, 1, 1)
+        return x + pos
+
 
 class SlotAttentionAutoEncoder(nn.Module):
-    def __init__(self, resolution = (128, 128), num_slots = 8, num_iterations = 3, decoder_initial_size = (8, 8), hidden_dim = 64, interpolate_mode = 'bilinear'):
+    def __init__(self, resolution = (128, 128), num_slots = 8, num_iterations = 3, decoder_initial_size = (8, 8), hidden_dim = 64, interpolate_mode = 'bilinear', position_encoding_layer = PositionEmbeddingImplicit):
         super().__init__()
         self.interpolate_mode = interpolate_mode
         self.resolution = resolution
@@ -184,7 +243,7 @@ class SlotAttentionAutoEncoder(nn.Module):
         self.hidden_dim = hidden_dim
         
         self.encoder_cnn = SlotAttentionEncoder(hidden_dim = self.hidden_dim)
-        self.encoder_pos = SoftPositionEmbed(self.hidden_dim)
+        self.encoder_pos = position_encoding_layer(self.hidden_dim)
         
         self.layer_norm = nn.LayerNorm(self.hidden_dim)
         self.mlp = nn.Sequential(
@@ -200,7 +259,7 @@ class SlotAttentionAutoEncoder(nn.Module):
             slot_size = self.hidden_dim,
             mlp_hidden_size = 128)
         
-        self.decoder_pos = SoftPositionEmbed(self.hidden_dim)
+        self.decoder_pos = position_encoding_layer(self.hidden_dim)
         self.decoder_cnn = SlotAttentionDecoder(hidden_dim = self.hidden_dim, output_dim = 4)
 
     def forward(self, image, slots = None):
